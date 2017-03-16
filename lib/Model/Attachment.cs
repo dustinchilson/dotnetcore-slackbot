c2156e758727ba6bf2f948eee04bd58736aad912
Dustin Chilson <dustinchilson@users.noreply.github.com>
2017-03-15 22:48:35 -0400

    Update to support Fields.

--- lib/Model/Attachment.cs
+++ lib/Model/Attachment.cs
@@ -1,4 +1,6 @@
-﻿namespace Slackbot.Model
+﻿using System.Collections.Generic;
+
+namespace Slackbot.Model
 {
     public class Attachment
     {
@@ -11,7 +13,7 @@
         public string Title { get; set; }
         public string Title_Link { get; set; }
         public string Text { get; set; }
-        public Field[] Fields { get; set; }
+        public List<Field> Fields { get; set; }
         public string Image_Url { get; set; }
         public string Thumb_Url { get; set; }
         public string Footer { get; set; }
